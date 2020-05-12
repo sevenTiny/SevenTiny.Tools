1c08a53c57734973fb4ce19d5bfb9a3fb501e043
wangdong3 <wangdong3@beisen.com>
2020-05-12 21:30:07 +0800

    add files

---
+++ CopyChildrenToTopOutPutFolder/CopyChildrenToTopOutPutFolder_Fx/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CopyChildrenToTopOutPutFolder_Fx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ignoreFile = new[]
+            {
+                ".git",
+                ".gitkeep",
+                "CopyChildrenToTopOutPutFolder_Fx.exe",
+                "CopyChildrenToTopOutPutFolder_Fx.exe.config",
+                "CopyChildrenToTopOutPutFolder_Fx.pdb"
+            };
+
+            try
+            {
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+                var outPutPath = Path.Combine(basePath, "OutPut");
+
+                if (Directory.Exists(outPutPath))
+                    Directory.Delete(outPutPath, true);
+
+                Directory.CreateDirectory(outPutPath);
+
+                var files = Directory.GetFiles(basePath, "*", SearchOption.AllDirectories);
+
+                int count = 0;
+
+                foreach (var item in files)
+                {
+                    if (ignoreFile.Any(t => item.Contains(t)))
+                        continue;
+
+                    var newPath = Path.Combine(outPutPath, Path.GetFileName(item));
+
+                    if (File.Exists(newPath))
+                        continue;
+
+                    File.Copy(item, newPath);
+
+                    count++;
+
+                    Console.WriteLine($"[{count}] Copy {item} -> {newPath}\n");
+                }
+
+                Console.WriteLine($"finished! {count} files copied.\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("execute error!\n");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("press any key to exit ...\n");
+            Console.ReadKey();
+        }
+    }
+}
